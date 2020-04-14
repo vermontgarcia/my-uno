3b438c34625fd7c0ef840153cb1a314cf852964f
Vermont Garcia <vermontgarcia@hotmail.com>
2020-04-14 01:02:37 -0400

    Table Component modified

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,21 +1,35 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import { connect } from 'react-redux';
-import firebase from 'firebase';
 import GameStatus from './GameStatus';
 import DrawPile from './DrawPile';
 import DiscarPile from './DiscardPile';
 import Hand from './Hand';
 import { styles } from './TableStyles';
-import { login, shoufleDeck, getHand } from '../actions';
-import { firebaseConfig } from '../config/appConfig';
-import { deck } from '../config/deck';
+import { login, shoufleDeck, getHand, updateDeck, updateHand, updateDiscardPile } from '../actions';
+import firebase from 'firebase';
 
 class Table extends Component {
 
   componentDidMount() {
-    firebase.initializeApp(firebaseConfig);
-    this.props.login();
+    console.log('DidMount')
+    //this.props.login();
+
+    // const room = 'myRoom'
+    // const rootRef = firebase.database().ref().child(`gameRooms/${room}`);
+
+    // const deckRef = rootRef.child('-M48FmYxmRBD6GEKQoVG/newDeck');
+    // deckRef.on('value', snapshot => {
+    //   updateDeck(snapshot.val())
+    // });
+    // const handRef = rootRef.child('-M48FmYxmRBD6GEKQoVG/newHand');
+    // handRef.on('value', snapshot => {
+    //   updateHand(snapshot.val())
+    // });
+    // const discardPileRef = rootRef.child('-M48FmYxmRBD6GEKQoVG/newDiscardPile');
+    // discardPileRef.on('value', snapshot => {
+    //   updateDiscardPile(snapshot.val())
+    // });
   }
 
   render() {
